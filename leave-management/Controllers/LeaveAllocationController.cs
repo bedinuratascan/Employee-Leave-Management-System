9c46a2c0f44036cb09650e78efe7c48dc3eac6a9
bedinuratascan <bedinuratascan@gmail.com>
2020-04-06 23:18:23 +0300

    Updated Database and Created Leave Allocation View

---
+++ leave-management/Controllers/LeaveAllocationController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using leave_management.Contracts;
+using leave_management.Data;
+using leave_management.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace leave_management.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    public class LeaveAllocationController : Controller
+    {
+        private readonly ILeaveAllocationRepository _leaveallocationrepo;
+        private readonly ILeaveTypeRepository _leaverepo;
+        private readonly IMapper _mapper;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public LeaveAllocationController(
+            ILeaveTypeRepository leaverepo,
+            IMapper mapper,
+            ILeaveAllocationRepository leaveallocationrepo,
+            UserManager<IdentityUser> userManager
+            )
+        {
+            this._leaverepo = leaverepo;
+            this._mapper = mapper;
+            this._leaveallocationrepo = leaveallocationrepo;
+            this._userManager = userManager;
+        }
+        // GET: LeaveAllocation
+        public ActionResult Index()
+        {
+            var leavetypes = _leaverepo.FindAll().ToList();
+            var mappedLeaveTypes = _mapper.Map<List<LeaveType>, List<LeaveTypeVM>>(leavetypes);
+            var model = new CreateLeaveAllocationVM
+            {
+                LeaveTypes = mappedLeaveTypes,
+                NumberUpdated = 0
+            };
+            return View(model);
+        }
+        public ActionResult SetLeave(int id)
+        {
+            var leavetype = _leaverepo.FindById(id);
+            var employees = _userManager.GetUsersInRoleAsync("Employee").Result;
+            foreach (var emp in employees)
+            {
+                if (_leaveallocationrepo.CheckAllocation(id, emp.Id))
+                    continue;
+                var allocation = new LeaveAllocationVM
+                {
+                    DateCreated = DateTime.Now,
+                    EmployeeId = emp.Id,
+                    LeaveTypeId = id,
+                    NumberOfDays = leavetype.DefaultDays,
+                    Period = DateTime.Now.Year                   
+                };
+                var leaveallocation = _mapper.Map<LeaveAllocation>(allocation);
+                _leaveallocationrepo.Create(leaveallocation);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: LeaveAllocation/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: LeaveAllocation/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LeaveAllocation/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LeaveAllocation/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: LeaveAllocation/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LeaveAllocation/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: LeaveAllocation/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- leave-management/Data/LeaveType.cs
+++ leave-management/Data/LeaveType.cs
@@ -10,9 +10,10 @@ namespace leave_management.Data
     {
         [Key]
         public int Id { get; set; }
-        [Required]
         public string Name { get; set; }
+        public int DefaultDays { get; set; }
         public DateTime DateCreated { get; set; }
 
     }
+
 }
